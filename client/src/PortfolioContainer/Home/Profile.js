32e24eb023e41acd0f95c6af513e103107bb476a
HamaSarbast-1997 <84290000+HamaSarbast-1997@users.noreply.github.com>
2022-01-04 16:23:45 +0300

    profile picture structure is finished

--- client/src/PortfolioContainer/Home/Profile.js
+++ client/src/PortfolioContainer/Home/Profile.js
@@ -6,23 +6,24 @@ export default function Profile() {
             <div className='profile-parent'>
                 <div className='profile-details'>
                     <div className='colz'>
-                        <a href='https://www.facebook.com/muhammed.sarbast.9'>
-                            <i className='fa fa-facebook-square'></i>
-                        </a>
-                        <a href='https://www.instagram.com/soulltan_.1997/'>
-                            <i className='fa fa-instagram'></i>
-                        </a>
-                        <a href='https://twitter.com/Muhamme28815866'>
-                            <i className='fa fa-twitter'></i>
-                        </a>
-                        <a href='https://github.com/HamaSarbast-1997'>
-                            <i class="fa fa-github"></i>
-                        </a>
-                        <a href='https://www.linkedin.com/in/muhammed-sarbast-358238202/'>
-                            <i class="fa fa-linkedin"></i>
-                        </a>
+                        <div className='colz-icon'>
+                            <a href='https://www.facebook.com/muhammed.sarbast.9'>
+                                <i className='fa fa-facebook-square'></i>
+                            </a>
+                            <a href='https://www.instagram.com/soulltan_.1997/'>
+                                <i className='fa fa-instagram'></i>
+                            </a>
+                            <a href='https://twitter.com/Muhamme28815866'>
+                                <i className='fa fa-twitter'></i>
+                            </a>
+                            <a href='https://github.com/HamaSarbast-1997'>
+                                <i class="fa fa-github"></i>
+                            </a>
+                            <a href='https://www.linkedin.com/in/muhammed-sarbast-358238202/'>
+                                <i class="fa fa-linkedin"></i>
+                            </a>
+                        </div>
                     </div>
-
                     <div className='profile-details-name'>
                         <span className='primary-text'>
                             {" "}
@@ -60,6 +61,9 @@ export default function Profile() {
                             <button className='btn highlighted-btn'>Get Resume</button>
                         </a>
                     </div>
+                </div>
+                <div className='profile-picture'>
+                    <div className='profile-picture-background'></div>
                 </div>
             </div>
         </div>
