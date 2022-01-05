4faded7e81186cddad751759a6f8f26d3b4f468d
HamaSarbast-1997 <84290000+HamaSarbast-1997@users.noreply.github.com>
2022-01-06 00:42:49 +0300

    rendaring component is finished

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,12 @@
 
 import './App.css';
-import Home from './PortfolioContainer/Home/Home';
+import PortfolioContainer from './PortfolioContainer/PortfolioContainer'
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <PortfolioContainer />
     </div>
   );
 }
-
-//hello
 export default App;
