5a4dd8d05ea7bed3c22850454e1eb3978c3bce72
HamaSarbast-1997 <84290000+HamaSarbast-1997@users.noreply.github.com>
2022-01-11 15:41:48 +0300

    about me section is created

---
+++ client/src/PortfolioContainer/AboutMe/AboutMe.js
@@ -0,0 +1,75 @@
+import React, { useEffect } from "react";
+import ScreenHeading from "../../utilities/ScreenHeading/ScreenHeading";
+import ScrollService from "../../utilities/ScrollService";
+import Animations from "../../utilities/Animations";
+import "./AboutMe.css";
+
+export default function AboutMe(props) {
+    let fadeInScreenHandler = (screen) => {
+        if (screen.fadeInScreen !== props.id) return;
+        Animations.animations.fadeInScreen(props.id);
+    };
+    const fadeInSubscription =
+        ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
+
+    const SCREEN_CONSTSANTS = {
+        description:
+            "Hi I am Muhammed and I am a Full Stack developer, focused on developing creative full stack web and apps. I'm Passionate about learning new things and building excellent software that improves the lives of people with the help of modern technology.",
+        highlights: {
+            bullets: [
+                "Full Stack web developer",
+                "React for front end",
+                "problem solver and critical thinker",
+                "Building REST API",
+                "Managing database",
+                "SQL and NoSQL for databse"
+            ],
+            heading: "Here are a Few Highlights:",
+        },
+    };
+    const renderHighlight = () => {
+        return SCREEN_CONSTSANTS.highlights.bullets.map((value, i) => (
+            <div className="highlight" key={i}>
+                <div className="highlight-blob"></div>
+                <span>{value}</span>
+            </div>
+        ));
+    };
+
+    return (
+        <div
+            className="about-me-container screen-container fade-in"
+            id={props.id || ""}
+        >
+            <div className="about-me-parent">
+                <ScreenHeading title={"About Me"} subHeading={"Why Choose Me?"} />
+                <div className="about-me-card">
+                    <div className="about-me-profile"></div>
+                    <div className="about-me-details">
+                        <span className="about-me-description">
+                            {SCREEN_CONSTSANTS.description}
+                        </span>
+                        <div className="about-me-highlights">
+                            <div className="highlight-heading">
+                                <span>{SCREEN_CONSTSANTS.highlights.heading}</span>
+                            </div>
+                            {renderHighlight()}
+                        </div>
+                        <div className="about-me-options">
+                            <button
+                                className="btn primary-btn"
+                                onClick={() => ScrollService.scrollHandler.scrollToHireMe()}
+                            >
+                                {" "}
+                                Hire Me{" "}
+                            </button>
+                            <a href="ehizcv.pdf" download="Ehiedu Ehizcv.pdf">
+                                <button className="btn highlighted-btn">Get Resume</button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
