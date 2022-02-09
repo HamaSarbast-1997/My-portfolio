c8d9e2b834da65ee9f992def562bb05dab43c9d8
HamaSarbast-1997 <84290000+HamaSarbast-1997@users.noreply.github.com>
2022-02-09 16:06:57 +0300

    fixed testimnial

--- client/src/PortfolioContainer/Testimonial/Testimonial.js
+++ client/src/PortfolioContainer/Testimonial/Testimonial.js
@@ -1,210 +1,211 @@
-import React from "react";
-import OwlCarousel from "react-owl-carousel";
+import React from 'react';
+import OwlCarousel from 'react-owl-carousel';
 import "owl.carousel/dist/assets/owl.carousel.css";
 import "owl.carousel/dist/assets/owl.theme.default.css";
-
-import ScreenHeading from "../../utilities/ScreenHeading/ScreenHeading";
-import ScrollService from "../../utilities/ScrollService";
-import Animations from "../../utilities/Animations";
-import shape from "../../assets/Testimonial/shape-bg.png";
-import "./Testimonial.css";
-
-
-
+import ScreenHeading from '../../utilities/ScreenHeading/ScreenHeading';
+import ScrollService from '../../utilities/ScrollService';
+import Animations from '../../utilities/Animations';
+import './Testimonial.css'
 
 export default function Testimonial(props) {
   let fadeInScreenHandler = (screen) => {
     if (screen.fadeInScreen !== props.id) return;
     Animations.animations.fadeInScreen(props.id);
   };
-
   const fadeInSubscription =
     ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
 
-  const options = {
-    loop: true,
-    margin: 0,
-    nav: true,
-    animateIn: "bounceInRight",
-    animateOut: "bounceOutRight",
-    dots: true,
-    autoplay: true,
-    smartSpeed: 1000,
-    responsive: {
-      0: {
-        items: 1,
-      },
-      768: {
-        items: 1,
-      },
-      1000: {
-        items: 3,
-      },
-    },
-  };
+    const options = {
+      loop:true,
+      margin:0,
+      nav:true,
+      animateIn:"bounceInRight",
+      animatdOut:"bounceOutRight",
+      dots:true,
+      autoplay:true,
+      smartSpeed:1000,
+      responsive:{
+        0:{
+          items:1
+        },
+        768:{
+          items:1
+        },
+        1000:{
+          items:3
+        }
+      }
+    }
 
   return (
     <div>
       <ScreenHeading
-        title={"Testimonial"}
-        subHeading={"What My Client Say About Me"}
+        title={'Testimonial'}
+        subHeading={'What Othere Developers Say About Me'}
       />
-      <section className="testimonial-section fade-in" id={props.id || ""}>
-        <div className="container">
-          <div className="row">
-            <OwlCarousel
-              className="owl-carousel"
-              id="testimonial-carousel"
-              {...options}
-            >
-              <div className="col-lg-12">
-                <div className="testi-item">
-                  <div className="testi-comment">
+      <section className='testimonial-section' id={props.id || ''}>
+        <div className='container'>
+          <div className='row'>
+            <OwlCarousel className='owl-carousel' id='testimonial-carousel' {...options}>
+
+
+              <div className='col-lg-12'>
+                <div className='testi-item'>
+                  <div className='testi-comment'>
                     <p>
-                      <i className="fa fa-quote-left" />
+                      <i className='fa fa-quote-left' />
                       I patronized Ehizeex and when He delivered, I honestly
                       fell in love with the project He is a very honest guy and
-                      he delivers ontime.
-                      <i className="fa fa-quote-right" />
+                      he delivers ontime
+                      <i className='fa fa-quote-right' />
                     </p>
-                    <ul className="stars list-unstyled">
+                    <ul className='stars list-unstyled'>
                       <li>
-                        <i className="fa fa-star" />
+                        <i className='fa fa-star' />
                       </li>
                       <li>
-                        <i className="fa fa-star" />
+                        <i className='fa fa-star' />
                       </li>
                       <li>
-                        <i className="fa fa-star" />
+                        <i className='fa fa-star' />
                       </li>
                       <li>
-                        <i className="fa fa-star-half-alt" />
+                        <i className='fa fa-half-alt' />
                       </li>
                       <li>
-                        <i className="fa fa-star" />
+                        <i className='fa fa-star' />
                       </li>
                     </ul>
                   </div>
-                  <div className="client-info">
-                    <img src='img/Testimonial/miran.png' alt="no internet connection"></img>
-                    <h5>Daisy Dominic</h5>
-                    <p>CEO InansGlobal</p>
+                  <div className='client-info'>
+                    <img src='img/Testimonial/miran.png' alt='No Internet Connection'
+                    ></img>
+                    <h5>Miran Kamal</h5>
+                    <p>Mobile Application Developer</p>
                   </div>
                 </div>
               </div>
 
-              <div className="col-lg-12">
-                <div className="testi-item">
-                  <div className="testi-comment">
+              <div className='col-lg-12'>
+                <div className='testi-item'>
+                  <div className='testi-comment'>
                     <p>
-                      <i className="fa fa-quote-left" />
-                      It was nice hiring this guy for my e-commerce project.He
-                      delivered even more than i can imagine. I recommend him
-                      for you.
-                      <i className="fa fa-quote-right" />
+                      <i className='fa fa-quote-left' />
+                      I patronized Ehizeex and when He delivered, I honestly
+                      fell in love with the project He is a very honest guy and
+                      he delivers ontime
+                      <i className='fa fa-quote-right' />
                     </p>
-                    <ul className="stars list-unstyled">
+                    <ul className='stars list-unstyled'>
                       <li>
-                        <i className="fa fa-star" />
+                        <i className='fa fa-star' />
                       </li>
                       <li>
-                        <i className="fa fa-star" />
+                        <i className='fa fa-star' />
                       </li>
                       <li>
-                        <i className="fa fa-star" />
+                        <i className='fa fa-star' />
                       </li>
                       <li>
-                        <i className="fa fa-star-half-alt" />
+                        <i className='fa fa-half-alt' />
                       </li>
                       <li>
-                        <i className="fa fa-star" />
+                        <i className='fa fa-star' />
                       </li>
                     </ul>
                   </div>
-                  <div className="client-info">
-                    <img src='img/Testimonial/miran.png' alt="no internet connection"></img>
-                    <h5>Mikel</h5>
-                    <p>CEO MikeTech</p>
+                  <div className='client-info'>
+                    <img src='img/Testimonial/miran.png' alt='No Internet Connection'
+                    ></img>
+                    <h5>Ahmed Nihmat</h5>
+                    <p>Mobile Application Developer</p>
                   </div>
                 </div>
               </div>
-              <div className="col-lg-12">
-                <div className="testi-item">
-                  <div className="testi-comment">
+
+              <div className='col-lg-12'>
+                <div className='testi-item'>
+                  <div className='testi-comment'>
                     <p>
-                      <i className="fa fa-quote-left" />
-                      When he delivered my job, my head spinned, it was so nice
-                      and he implemented every functionality i requested for.
-                      Thank you.
-                      <i className="fa fa-quote-right" />
+                      <i className='fa fa-quote-left' />
+                      I patronized Ehizeex and when He delivered, I honestly
+                      fell in love with the project He is a very honest guy and
+                      he delivers ontime
+                      <i className='fa fa-quote-right' />
                     </p>
-                    <ul className="stars list-unstyled">
+                    <ul className='stars list-unstyled'>
                       <li>
-                        <i className="fa fa-star" />
+                        <i className='fa fa-star' />
                       </li>
                       <li>
-                        <i className="fa fa-star" />
+                        <i className='fa fa-star' />
                       </li>
                       <li>
-                        <i className="fa fa-star" />
+                        <i className='fa fa-star' />
                       </li>
                       <li>
-                        <i className="fa fa-star-half-alt" />
+                        <i className='fa fa-half-alt' />
                       </li>
                       <li>
-                        <i className="fa fa-star" />
+                        <i className='fa fa-star' />
                       </li>
                     </ul>
                   </div>
-                  <div className="client-info">
-                    <img src='img/Testimonial/miran.png' alt="no internet connection"></img>
-                    <h5>Daisy Dominic</h5>
-                    <p>CEO InansGlobal</p>
+                  <div className='client-info'>
+                    <img src='img/Testimonial/miran.png' alt='No Internet Connection'
+                    ></img>
+                    <h5>Ali Amjad</h5>
+                    <p>Mobile Application Developer</p>
                   </div>
                 </div>
               </div>
-              <div className="col-lg-12">
-                <div className="testi-item">
-                  <div className="testi-comment">
+
+              <div className='col-lg-12'>
+                <div className='testi-item'>
+                  <div className='testi-comment'>
                     <p>
-                      <i className="fa fa-quote-left" />
-                      This guy is incredibly awesome, I hired him and when He
-                      delivered, I honestly fell in love with the project. He is
-                      indeed a great guy,
-                      <i className="fa fa-quote-right" />
+                      <i className='fa fa-quote-left' />
+                      I patronized Ehizeex and when He delivered, I honestly
+                      fell in love with the project He is a very honest guy and
+                      he delivers ontime
+                      <i className='fa fa-quote-right' />
                     </p>
-                    <ul className="stars list-unstyled">
+                    <ul className='stars list-unstyled'>
                       <li>
-                        <i className="fa fa-star" />
+                        <i className='fa fa-star' />
                       </li>
                       <li>
-                        <i className="fa fa-star" />
+                        <i className='fa fa-star' />
                       </li>
                       <li>
-                        <i className="fa fa-star" />
+                        <i className='fa fa-star' />
                       </li>
                       <li>
-                        <i className="fa fa-star-half-alt" />
+                        <i className='fa fa-half-alt' />
                       </li>
                       <li>
-                        <i className="fa fa-star" />
+                        <i className='fa fa-star' />
                       </li>
                     </ul>
                   </div>
-                  <div className="client-info">
-                    <img src='img/Testimonial/miran.png' alt="no internet connection"></img>
-                    <h5>John Smith</h5>
-                    <p>Bnaker</p>
+                  <div className='client-info'>
+                    <img src='img/Testimonial/miran.png' alt='No Internet Connection'
+                    ></img>
+                    <h5>Aram Rafiq</h5>
+                    <p>Mobile Application Developer</p>
                   </div>
                 </div>
               </div>
+
+
             </OwlCarousel>
           </div>
         </div>
       </section>
       <div className="footer-image">
-        <img src={shape} alt="Phot0 not responding" />
+        {/* <img src={shape} alt="Phot0 not responding" /> */}
       </div>
     </div>
-  );
+
+  )
 }
